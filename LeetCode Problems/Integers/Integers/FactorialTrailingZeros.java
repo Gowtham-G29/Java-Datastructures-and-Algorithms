ea4297bf747c95d54bc5e52aa4f58bab2a7c26f6
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-04-05 15:17:06 +0530

    #172

--- LeetCode Problems/Integers/Integers/FactorialTrailingZeros.java
+++ LeetCode Problems/Integers/Integers/FactorialTrailingZeros.java
@@ -6,7 +6,8 @@ class Solution {
             count+=(n/currentPowerOfFive);
             currentPowerOfFive*=5;
         }
-
+        // The number of trailing zeros in n! is equal to the number of times 5 is a factor in the numbers from 1 to n.
+        // This is because there are always more factors of 2 than factors of 5 in the factorial.
         return count;
         
     }
